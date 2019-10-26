6cfe06e7592860f48b75e99ce78e04327e5d45ab
jmrada14 <jmrada14@tutanota.com>
2019-10-26 09:15:00 -0400

    passport is needed

--- models/user.js
+++ models/user.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const passportLocalMongoose = require('passport-local-mongoose');
 
 const UserSchema = new Schema({
     username: {String, required: true},
     password:{String, required: true}
 });
+/*
+still need passport config
+ */
 
-// UserSchema.plugin(passportLocalMongoose);
 const User = mongoose.model('User', UserSchema);
 module.exports = User;
