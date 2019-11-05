f94d83cff4f688457da7e3d2026a1276ff369a4c
jmrada14 <jmrada14@tutanota.com>
2019-11-05 17:37:41 -0500

    bill by specific id

--- routes/GetBills.js
+++ routes/GetBills.js
@@ -1,11 +1,12 @@
 const axios = require('axios');
-const subject = "climate pollution environment";
+const congress = "whatever";
+const bills = "whatever";
 const db = require("../models/bills");
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/ground-up');
 ////////////////////////
 axios.request({
-    url: `https://api.propublica.org/congress/v1/bills/search.json?query=${subject}`,
+    url: `https://api.propublica.org/congress/v1/${congress}/bills/${billId}.json`,
     headers: { 'X-API-Key': '9q67HMZ2ly0hOrXWjT9l31WN6TiGuh7XrkBl1vC' },
     method: 'get'
 }).then(response => {
