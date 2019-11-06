7c99f9d555d98db68d2510f656629a8a3899ba3c
jmrada14 <jmrada14@tutanota.com>
2019-11-06 18:30:38 -0500

    auth done

--- models/user.js
+++ models/user.js
@@ -1,13 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+// Create Schema
 const UserSchema = new Schema({
-    username: {String, required: true},
-    password:{String, required: true}
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
 });
-/*
-still need passport config
- */
-
-const User = mongoose.model('User', UserSchema);
+User = mongoose.model("users", UserSchema);
 module.exports = User;
