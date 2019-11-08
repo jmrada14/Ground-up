36a4bb359b3df012ee4afb2c96ce3d187dc474c5
jmrada14 <jmrada14@tutanota.com>
2019-11-08 09:26:10 -0500

    bills controller with some functional routes, still need connection with the original server, but tested them and they work

---
+++ routes/api/bills.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const billsController = require("../../controllers/billsController");
+
+
+router.route("/all")
+    .get(billsController.findAllBills);
+
+router.route("/:id")
+    .get(billsController.findByBillId);
+
+router.route("/:title")
+    .get(billsController.findByTitle);
+
+router.route("/:sponsorState")
+    .get(billsController.findBySponsorState);
+
+router.route("/:sponsorParty")
+    .get(billsController.findBySponsorParty);
+
+router.route("/:introDate")
+    .get(billsController.findByIntroducedDate);
+
+
+
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
 const repRoutes = require("./reps");
 const userRoutes = require("./users");
+const billRoutes = require("./bills");
 
 // Book routes
 router.use("/members", repRoutes);
 router.use("/users", userRoutes);
+router.use("/bills", billRoutes);
+
 module.exports = router;
