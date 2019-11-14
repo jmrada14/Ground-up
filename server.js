20659dc644918c40771364e53f3286cec37c6e0a
jmrada14 <jmrada14@tutanota.com>
2019-11-13 21:34:36 -0500

    Test

--- server.js
+++ server.js
@@ -16,6 +16,14 @@ app.use(
 );
 app.use(bodyParser.json());
 
+
+if (process.env.NODE_ENV === "production") {
+
+    app.use(express.static("client/build"));
+
+}
+
+
 // DB Config
 const db = require("./config/keys").mongoURI;
 
