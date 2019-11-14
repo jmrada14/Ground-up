0f62336decf1b98af271e659277a4bb75e64272d
Leolal <leolal1984@icloud.com>
2019-11-14 17:36:30 -0500

    Took database off the app

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: process.env.MONGODB_URI ||"mongodb://localhost:27017/ground-up-test",
+  mongoURI: process.env.MONGODB_URI ||"mongodb://localhost:27017/ground-up",
   secretOrKey: "secret"
 };
