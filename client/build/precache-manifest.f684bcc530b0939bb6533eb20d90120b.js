327c82dddbd414dc05cd869849787e073a80948a
jmrada14 <jmrada14@tutanota.com>
2019-11-13 21:09:52 -0500

    test

---
+++ client/build/precache-manifest.f684bcc530b0939bb6533eb20d90120b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3e1a493a20c91cf138d6",
+    "url": "/static/js/main.3e1a493a.chunk.js"
+  },
+  {
+    "revision": "30ec17e6ce377e20a609",
+    "url": "/static/js/1.30ec17e6.chunk.js"
+  },
+  {
+    "revision": "3e1a493a20c91cf138d6",
+    "url": "/static/css/main.0b1a21a6.chunk.css"
+  },
+  {
+    "revision": "0b1ecb6f29734a2917723c548b6f4052",
+    "url": "/index.html"
+  }
+];
